71343dc1d5cc5a0f1c3aa27680433fd18b4ed53f
Masterluke <master.luke87@gmail.com>
2021-12-05 14:16:26 +0100

    Added possiblity to set a target spread

--- permRand.py
+++ permRand.py
@@ -1,6 +1,7 @@
 import numpy as np
 import argparse
 
+
 def flushArrayToFile(X,fName,sep=""):
     f = open(fName,"w")
     for i in X:
@@ -9,8 +10,45 @@ def flushArrayToFile(X,fName,sep=""):
         f.write("\n")
     f.close()
 
+def calcSpread(A,M):
+    u, counts = np.unique(A, return_counts=True)
+    if len(u)<M:
+        return np.max(counts)
+    else:
+        return np.max(counts) - np.min(counts)
+
+
+def limitSpread(M,k,N,constCombs,randomSampleSize,tSpread):
+    if not hasattr(sampleUnderConstraint,"RND"):
+        sampleUnderConstraint.RND=np.random.default_rng()
+
+    X = np.array([sampleUnderConstraint.RND.choice(range(M), (k), replace=False) for x in range(randomSampleSize)])
+    for i in X:
+        unique = True
+        for j in constCombs:
+            if len(np.intersect1d(i, j))>1:
+                unique = False
+        if unique==True:
+            if constCombs:
+                oldDist   = np.array(constCombs)
+                oldSpread = calcSpread(oldDist,M)
+                newDist   = np.array(np.append(constCombs,i))
+                newSpread = calcSpread(newDist,M)
+                print("Old Spread: {} , New Spread: {}  ".format(oldSpread,newSpread),end='')
+                if (newSpread<=tSpread):
+                     constCombs.append(i)
+                print("Progress: {}/{}".format(len(constCombs),N))
+            else:
+                constCombs.append(i)
+            if len(constCombs)==N:
+                return True
+    return False
+
 def sampleUnderConstraint(M,k,N,constCombs,randomSampleSize):
-    X = np.array([np.random.choice(range(M), (k), replace=False) for x in range(randomSampleSize)])
+    if not hasattr(sampleUnderConstraint,"RND"):
+        sampleUnderConstraint.RND=np.random.default_rng()
+
+    X = np.array([sampleUnderConstraint.RND.choice(range(M), (k), replace=False) for x in range(randomSampleSize)])
     for i in X:
         unique = True
         for j in constCombs:
@@ -43,6 +81,8 @@ if __name__ == "__main__":
                     help='Defines the number of permutations')
     parser.add_argument('-k', metavar='k',default=3, type=int,
                     help='Defines how many "ones" occur in the arrays')
+    parser.add_argument('-t', metavar='t',default=-1, type=int,
+                    help='Set a maximum taget spread')
     parser.add_argument('-maxRounds', metavar='maxR',default=1000, type=int,
                     help='Set number of Cycles to find permutations')
     parser.add_argument('-radomSampleSize', metavar='randS',default=10000, type=int,
@@ -60,11 +100,19 @@ if __name__ == "__main__":
     randomSampleSize = args.radomSampleSize
     arrOut = args.arrOUT
     idxOut = args.idxOUT
+    tSpread = args.t
 
     constCombs = [ ]
-    for l in range(maxRounds):
-        if sampleUnderConstraint(M, k, N, constCombs,randomSampleSize):
-            break
+
+    if tSpread!=-1:
+        for l in range(maxRounds):
+            if limitSpread(M, k, N, constCombs, randomSampleSize,tSpread):
+                break
+    else:
+        for l in range(maxRounds):
+            if sampleUnderConstraint(M, k, N, constCombs, randomSampleSize):
+                break
+
     if len(constCombs)<N:
         print("WARNING!! We were not able to find enough uniqe lists.")
     
@@ -77,6 +125,10 @@ if __name__ == "__main__":
         if gridCounter==grid.shape[0]:
             break    
     
+    distribution = np.sum(grid,axis=0)
+    print("Min: {} ;Max: {};Spread: {};".format(np.min(distribution),
+    np.max(distribution),np.max(distribution)-np.min(distribution)))
+
     if arrOut:
         flushArrayToFile(grid, arrOut)
         print("Array written to file: {}".format(arrOut),sep="")
